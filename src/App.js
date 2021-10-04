9f8c63a06266813d26be456547b1729f72578783
James Chou <jamesmtchou@gmail.com>
2021-10-04 00:02:04 -0700

    feat(bio): update profile description

--- src/App.js
+++ src/App.js
@@ -12,11 +12,10 @@ class App extends Component {
                     Francisco, CA.
                 </p>
                 <p>
-                    I am working at <strong><a href="http://www.accenture.com" target="_blank" id="accenture-link">Accenture</a></strong> as a Technology Consultant. My current client is <strong><a href="https://www.autodesk.com" target="_blank" id="autodesk-link">Autodesk</a></strong>. I've also worked at <strong><a href="https://www.microsoft.com/" target="_blank"
-                                                                 id="microsoft-link">Microsoft</a></strong> on internal IAM systems.
+                    I am working at <strong><a href="http://www.apple.com" target="_blank" id="apple-link">Apple</a></strong> as a Sr. Software Engineer in the Special Projects Group. Previously, I've worked at <strong><a href="https://www.getcruise.com" target="_blank" id="cruise-link">Cruise</a></strong>, <strong><a href="https://www.autodesk.com" target="_blank" id="autodesk-link">Autodesk</a></strong>, and <strong><a href="https://www.microsoft.com/" target="_blank" id="microsoft-link">Microsoft</a></strong>.
                 </p>
                 <p>
-                    I primarily do JavaScript, CSS, HTML, Python, and Java here and there. I also produced <strong><a
+                    I primarily code in ES6, CSS3, HTML5, Python, Golang, SQL, and C++ here and there. I also produced <strong><a
                     href="https://www.youtube.com/watch?v=ejJ8AiGy2aI" target="_blank"
                     id="video-link">shows</a></strong> for the Taiwanese American community. Finally, I have an adorable poodle who sometimes posts his cute <strong><a href="http://instagram.com/poo_poo_littlered" target="_blank" id="instagram-link">selfies</a></strong>.
                 </p>
